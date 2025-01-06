b811b9c9431767e1e661867f43a0bc0c9b52f8fa
Gaspard <gaspard_hedde@yahoo.com>
2025-01-06 17:19:27 +0100

    test url

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ export default {
     async search(query) {
       try {
         this.tracks = [];
-        const apiBaseUrl = process.env.VUE_APP_API_BASE_URL || 'http://localhost:3000';
+        const apiBaseUrl = process.env.VUE_APP_API_BASE_URL || 'http://localhost:8000';
         const response = await axios.get(`${apiBaseUrl}/api/search?q=${query}`);
         this.tracks = response.data.data;
         this.hasSearched = true;
